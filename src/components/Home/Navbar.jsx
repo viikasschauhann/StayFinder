bc5b84d353a1b184d677b471724fe102cba37789
viikasschauhann <vchauhan6155@gmail.com>
2025-07-31 00:31:05 +0530

    adding files

---
+++ src/components/Home/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Navbar() {
+    
+    const NavLinks = {
+        Home: "",
+        Experiences: "", 
+        Services: "",
+    }
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
